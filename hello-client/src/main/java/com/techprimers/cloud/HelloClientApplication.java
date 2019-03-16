b747dfce88c7c8a6cf7063e2d8a47484e1beda39
Deepak <Deepak@DESKTOP-MHVD92A>
2019-03-16 11:32:00 +0530

    hello-client first commit

---
+++ hello-client/src/main/java/com/techprimers/cloud/HelloClientApplication.java
@@ -0,0 +1,32 @@
+package com.techprimers.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableFeignClients
+public class HelloClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloClientApplication.class, args);
+	}
+
+}
+
+
+
+@Configuration
+class Config {
+
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
