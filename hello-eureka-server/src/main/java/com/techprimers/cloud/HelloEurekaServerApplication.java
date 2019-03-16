a2a20b5d5ca0a46c8cee8354c465408f8fd5d84c
Deepak <Deepak@DESKTOP-MHVD92A>
2019-03-16 11:34:29 +0530

    hello-eureka-server first commit

---
+++ hello-eureka-server/src/main/java/com/techprimers/cloud/HelloEurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.techprimers.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class HelloEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEurekaServerApplication.class, args);
+	}
+
+}
+
